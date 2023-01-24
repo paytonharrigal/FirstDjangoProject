5247d304346db97cdc7446a62ece241a42a28d21
paytonharrigal <111763320+paytonharrigal@users.noreply.github.com>
2023-01-23 22:36:49 -0500

    Update 3

--- Django_Project/try_django/articles/views.py
+++ Django_Project/try_django/articles/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render
 from .models import Article 
 # Create your views here.
 
+def article_search_view(request):
+
+    
+    context = {
+
+    }
+
+    return render(request, "articles/search.html", context=context)
+
 def article_detail_view(request, id= None):
     article_obj = None
     if id is not None:
